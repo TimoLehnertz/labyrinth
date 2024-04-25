b5fccb42e02c5937665743c3c950c263d543e498
TimoLehnertz <57093983+TimoLehnertz@users.noreply.github.com>
2024-04-25 17:40:23 +0200

    stuff

--- src/app/[locale]/friends/page.tsx
+++ src/app/[locale]/friends/page.tsx
@@ -1,7 +1,6 @@
 import { redirect } from "next/navigation";
 import React from "react";
 import AddFriendsForm from "./AddFriendsForm";
-import Chat from "../../_components/Chat";
 import { server } from "../../serverAPI";
 
 export default async function Page() {
